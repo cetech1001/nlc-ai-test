aa6fd8d63134bce4d07d74e642fb7a8de5332f4f
Chiemerie <alexemerie7@gmail.com>
2025-06-17 01:47:31 +0100

    Modified Dockerfiles and next config files to suit the environment

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -8,15 +8,35 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  // Use this to set Nx-specific options
-  // See: https://nx.dev/recipes/next/next-config-setup
-  nx: {},
+  output: 'standalone',
+
+  nx: {
+    svgr: false,
+  },
+
+  /*experimental: {
+    optimizeCss: true,
+  },*/
+
+  images: {
+    domains: [],
+  },
+
+  env: {},
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 const plugins = [
-  // Add more Next.js plugins to this list if needed.
   withNx,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
-
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,22 +1,42 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+const {composePlugins, withNx} = require('@nx/next');
 
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  // Use this to set Nx-specific options
-  // See: https://nx.dev/recipes/next/next-config-setup
-  nx: {},
+  output: 'standalone',
+
+  nx: {
+    svgr: false,
+  },
+
+  /*experimental: {
+    optimizeCss: true,
+  },*/
+
+  images: {
+    domains: [],
+  },
+
+  env: {},
+
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 const plugins = [
-  // Add more Next.js plugins to this list if needed.
   withNx,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
-
