017ee660f952a961eeddbb56cf49904009584c3d
Chiemerie <alexemerie7@gmail.com>
2025-07-08 14:55:53 +0100

    Added updated layout file

--- apps/admin/src/app/(dashboard)/layout.tsx
+++ apps/admin/src/app/(dashboard)/layout.tsx
@@ -47,7 +47,11 @@ const AdminDashboardLayout = ({ children }: DashboardLayoutProps) => {
 
   return (
     <div className="min-h-screen bg-[#000000]">
-      <SidebarComponent navigateTo={navigateTo} menuItems={menuItems} logout={handleLogout} />
+      <SidebarComponent
+        pathname={pathname}
+        navigateTo={navigateTo}
+        menuItems={menuItems}
+        logout={handleLogout} />
 
       <div className="lg:pl-72">
         <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-[#1A1A1A] bg-[#0A0A0A] px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
