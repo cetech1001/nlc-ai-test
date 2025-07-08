f6d72975e9ddd088194f08e15da2d3f046b4eb59
Chiemerie <alexemerie7@gmail.com>
2025-07-08 23:31:11 +0100

    Made slight change in coach app to trigger deployment

--- apps/coach/src/app/(dashboard)/home/page.tsx
+++ apps/coach/src/app/(dashboard)/home/page.tsx
@@ -1,6 +1,11 @@
+import { useAuth } from "@nlc-ai/auth";
+import {USER_TYPE} from "@nlc-ai/types";
+
 const CoachHomePage = () => {
+  const { user } = useAuth(USER_TYPE.coach);
+
   return (
-    <div>Hello Coach!</div>
+    <div>Hello {user?.firstName}!</div>
   );
 }
 
