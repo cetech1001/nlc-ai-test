4374c45d5da821c69fded15d7eb61987cd1a7730
Chiemerie <alexemerie7@gmail.com>
2025-08-31 12:53:51 +0100

    Replaced redirect with router.push

--- apps/web/pre-launch/src/lib/components/home/hero.section.tsx
+++ apps/web/pre-launch/src/lib/components/home/hero.section.tsx
@@ -1,7 +1,9 @@
+'use client';
 import Image from "next/image";
-import {redirect} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export const HeroSection = () => {
+    const router = useRouter();
 
     return (
         <>
@@ -10,7 +12,7 @@ export const HeroSection = () => {
                     <div className="flex items-center justify-between w-full space-x-3 sm:px-8">
                         <Image src={'/images/logo-large.png'} height={192} width={192} alt={'Logo'}/>
                         <button onClick={() => {
-                            redirect(process.env.NEXT_PUBLIC_COACH_PLATFORM_URL || '');
+                            router.push(process.env.NEXT_PUBLIC_COACH_PLATFORM_URL || '/');
                         }} className="btn-primary">
                             <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/10 to-transparent translate-x-[-100%] group-hover:translate-x-[100%] transition-transform duration-700"></div>
 
